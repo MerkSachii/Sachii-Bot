b2105e27691f5b28b38fbd284ced39af63ffda3b
Mark Sanchez <dlsu.sachii@gmail.com>
2017-06-25 17:24:26 +0800

    Simple posting code

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import twitter
+
+
+def main():
+    message = 'Testing testing 123'
+    api = twitter.Api(consumer_key='kWTqtf716ZGDUvw9YEXbSU7o3',
+                      consumer_secret='g6sIaAm9Ku5LbsC28Ub7hnvvBTznVjdzzKprCgVqnixTGR4E9G',
+                      access_token_key='878885859772162049-3jrDmGf8sxb7rs7b8HvYpUrpCl3kjF3',
+                      access_token_secret='G9XteyJXBmdeNJfDURScrf8DVZVYSCEy8Ug7IfrqNnVuC',)
+    try:
+        status = api.PostUpdate(message)
+    except UnicodeDecodeError:
+        print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
+        print("Try explicitly specifying the encoding with the --encoding flag")
+        sys.exit(2)
+    print("%s just posted: %s" % (status.user.name, status.text))
+
+
+if __name__ == "__main__":
+    main()
